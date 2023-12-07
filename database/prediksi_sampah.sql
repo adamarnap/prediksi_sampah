3cf00dc44533f91067c24755da7aed256b41f035
adamarnap <adamarnap0117@gmail.com>
2023-12-07 19:01:01 +0700

    Upload db

--- database/prediksi_sampah.sql
+++ database/prediksi_sampah.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.5.1 (64 bit)
-MySQL - 5.7.25 : Database - base_code_igniter
+MySQL - 5.7.25 : Database - prediksi_sampah
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.7.25 : Database - base_code_igniter
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`base_code_igniter` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prediksi_sampah` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `base_code_igniter`;
+USE `prediksi_sampah`;
 
 /*Table structure for table `mst_app_data` */
 
@@ -46,7 +46,7 @@ CREATE TABLE `mst_app_data` (
 /*Data for the table `mst_app_data` */
 
 insert  into `mst_app_data`(`app_data_id`,`portal_id`,`logo_app`,`no_tlp_app`,`email_app`,`no_whatsapp_app`,`no_telegram_app`,`instagram_app`,`facebook_app`,`tiktok_app`,`twitter_app`,`github_app`,`linkedin_app`,`youtube_app`,`mdd`,`mdb`,`mdb_created`) values 
-(1,'01','12.png','083850010004','sarjanasipil.education@gmail.com','083850010004','083850010004','sarjanasipil.ed','-','sarjanasipil.ed','-','-','-','-','2023-07-27 10:59:51','230621000001','adminbase');
+(1,'01','12.png','083850010004','predsampah@mail.com','083850010004','083850010004','-','-','sarjanasipil.ed','-','-','-','-','2023-07-27 10:59:51','230621000001','adminbase');
 
 /*Table structure for table `mst_email` */
 
@@ -134,7 +134,11 @@ insert  into `mst_menu`(`menu_id`,`portal_id`,`menu_level`,`menu_induk`,`menu_ju
 ('1689870222','01','submenu','02','Email Verifikasi','Email aktifasi akun pengguna dan verifikasi reset password','setting/sistem/email',6,'1','1','-','2306210000','adminbase','2023-07-20 23:23:42'),
 ('1689949241','01','submenu','02','Data Aplikasi','Data-data Aplikasi','setting/sistem/app_data',7,'1','1','-','2306210000','adminbase','2023-07-21 21:20:41'),
 ('1690431550','01','submenu','02','Rekening Bank','Data rekening bank perusahaan','setting/sistem/rekening',8,'1','1','-','2306210000','adminbase','2023-07-27 11:38:52'),
-('1690431782','01','submenu','1690431344','Transaksi Paket','Data transaksi pembeli paket','transaksi/keuangan/transaksi_paket',1,'1','1','-','2306210000','adminbase','2023-07-27 11:23:02');
+('1690431782','01','submenu','1690431344','Transaksi Paket','Data transaksi pembeli paket','transaksi/keuangan/transaksi_paket',1,'1','1','-','2306210000','adminbase','2023-07-27 11:23:02'),
+('1701856865','01','tunggal','0','Data Sampah','-','master/data/sampah',4,'1','1','fa fa-trash','2306210000','adminbase','2023-12-06 17:01:05'),
+('1701856974','01','tunggal','0','Data Daerah','-','master/data/daerah',2,'1','1','fa fa-globe','2306210000','adminbase','2023-12-06 17:02:54'),
+('1701857038','01','tunggal','0','Data Sungai','-','master/data/sungai',3,'1','1','fa fa-globe','2306210000','adminbase','2023-12-06 17:03:58'),
+('1701879862','01','tunggal','0','Data Prediksi','-','master/data/prediksi',5,'1','1','fa fa-search-plus','2306210000','adminbase','2023-12-06 23:24:22');
 
 /*Table structure for table `mst_portal` */
 
@@ -156,7 +160,7 @@ CREATE TABLE `mst_portal` (
 /*Data for the table `mst_portal` */
 
 insert  into `mst_portal`(`portal_id`,`portal_nm`,`site_title`,`site_desc`,`meta_desc`,`meta_keyword`,`mdb`,`mdb_name`,`mdd`) values 
-('01','Base CI Arnap','Base Code Igniter Arnap','Base Code Igniter 3 Arnap','Base Code Igniter 3 Arnap','Base Code Igniter 3 Arnap','2306210000','adminbase','2023-07-27 10:53:16');
+('01','Prediksi Sampah','Base Code Igniter Arnap','PREDIKSI VOLUME TPA - KAB. HULU SUNGAI SELATAN, KALIMANTAN SELATAN','PREDIKSI VOLUME TPA - KAB. HULU SUNGAI SELATAN, KALIMANTAN SELATAN','PREDIKSI VOLUME TPA - KAB. HULU SUNGAI SELATAN, KALIMANTAN SELATAN','2306210000','adminbase','2023-07-27 10:53:16');
 
 /*Table structure for table `mst_role` */
 
@@ -214,7 +218,11 @@ insert  into `mst_role_menu`(`role_id`,`menu_id`,`role_menu_akses`) values
 ('01','1689870222','1'),
 ('01','1689949241','1'),
 ('01','1690431550','1'),
-('01','1690431782','1');
+('01','1690431782','1'),
+('01','1701856865','1'),
+('01','1701856974','1'),
+('01','1701857038','1'),
+('01','1701879862','1');
 
 /*Table structure for table `mst_role_user` */
 
@@ -289,6 +297,127 @@ CREATE TABLE `mst_user` (
 insert  into `mst_user`(`user_id`,`user_alias`,`user_nama`,`user_pass`,`user_mail`,`user_img_name`,`user_st`,`tgl_lahir`,`tempat_lahir`,`alamat`,`no_tlp`,`no_whatsapp`,`no_telegram`,`facebook`,`instagram`,`twitter`,`mdb`,`mdb_name`,`mdd`) values 
 ('230621000001','Admin Bases','adminbase','$2y$10$HFP0uf0A0IFmFGjuiu4W0eg0nhxMCW2b5DfPB3onBd0ajRVHg2As6','adminbase123@mail.com','230621000001.jpg','1','2023-07-19','Yogyakarta','Condong Catur, Depok, Sleman, Yogyakarta','082456887634','0824568876342','0824568876','admin','admin','admin','2306210000','adminbase','2023-07-19 23:55:36'),
 ('230721000001','DEBOK GEDANG','debokgedang','$2y$10$/x9zJAx..A039dxjRJTjpeu1d2hmYAwDIRM2JDI6vZf8fxSTEJVhG','debokgedang405@gmail.com','default.png','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2023-07-21 11:44:11');
+
+/*Table structure for table `tb_daerah` */
+
+DROP TABLE IF EXISTS `tb_daerah`;
+
+CREATE TABLE `tb_daerah` (
+  `id_daerah` int(11) NOT NULL,
+  `desa` varchar(50) DEFAULT NULL,
+  `kecamatan` varchar(50) DEFAULT NULL,
+  `kab_kota` varchar(50) DEFAULT NULL,
+  `provinsi` varchar(50) DEFAULT NULL,
+  `status` enum('aktif','nonaktif') DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `created_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_daerah`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_daerah` */
+
+insert  into `tb_daerah`(`id_daerah`,`desa`,`kecamatan`,`kab_kota`,`provinsi`,`status`,`created`,`created_name`) values 
+(1,'-','-','Hulu Sungai Selatan','Kalimantan Selatan','aktif','2023-12-06 16:43:53','adminbase');
+
+/*Table structure for table `tb_prediksi` */
+
+DROP TABLE IF EXISTS `tb_prediksi`;
+
+CREATE TABLE `tb_prediksi` (
+  `id_prediksi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sungai` int(11) DEFAULT NULL,
+  `tahun_prediksi` varchar(10) DEFAULT NULL,
+  `data_prediksi` text,
+  `created` datetime DEFAULT NULL,
+  `created_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_prediksi`),
+  KEY `id_sungai` (`id_sungai`),
+  CONSTRAINT `tb_prediksi_ibfk_2` FOREIGN KEY (`id_sungai`) REFERENCES `tb_sungai` (`id_sungai`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_prediksi` */
+
+insert  into `tb_prediksi`(`id_prediksi`,`id_sungai`,`tahun_prediksi`,`data_prediksi`,`created`,`created_name`) values 
+(3,1,'2024','[{\"vol_predicted\":1160.8,\"bulan_predicted\":\"Januari\"},{\"vol_predicted\":1161.77,\"bulan_predicted\":\"Februari\"},{\"vol_predicted\":1162.75,\"bulan_predicted\":\"Maret\"},{\"vol_predicted\":1163.73,\"bulan_predicted\":\"April\"},{\"vol_predicted\":1164.7,\"bulan_predicted\":\"Mei\"},{\"vol_predicted\":1165.68,\"bulan_predicted\":\"Juni\"},{\"vol_predicted\":1166.66,\"bulan_predicted\":\"Juli\"},{\"vol_predicted\":1167.63,\"bulan_predicted\":\"Agustus\"},{\"vol_predicted\":1168.61,\"bulan_predicted\":\"September\"},{\"vol_predicted\":1169.59,\"bulan_predicted\":\"Oktober\"},{\"vol_predicted\":1170.57,\"bulan_predicted\":\"November\"},{\"vol_predicted\":1171.54,\"bulan_predicted\":\"Desember\"}]','2023-12-07 16:15:46','adminbase'),
+(4,1,'2022','[{\"vol_predicted\":1137.35,\"bulan_predicted\":\"Januari\"},{\"vol_predicted\":1138.33,\"bulan_predicted\":\"Februari\"},{\"vol_predicted\":1139.3,\"bulan_predicted\":\"Maret\"},{\"vol_predicted\":1140.28,\"bulan_predicted\":\"April\"},{\"vol_predicted\":1141.26,\"bulan_predicted\":\"Mei\"},{\"vol_predicted\":1142.23,\"bulan_predicted\":\"Juni\"},{\"vol_predicted\":1143.21,\"bulan_predicted\":\"Juli\"},{\"vol_predicted\":1144.19,\"bulan_predicted\":\"Agustus\"},{\"vol_predicted\":1145.17,\"bulan_predicted\":\"September\"},{\"vol_predicted\":1146.14,\"bulan_predicted\":\"Oktober\"},{\"vol_predicted\":1147.12,\"bulan_predicted\":\"November\"},{\"vol_predicted\":1148.1,\"bulan_predicted\":\"Desember\"}]','2023-12-07 16:37:28','adminbase');
+
+/*Table structure for table `tb_sampah` */
+
+DROP TABLE IF EXISTS `tb_sampah`;
+
+CREATE TABLE `tb_sampah` (
+  `id_sampah` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sungai` int(11) DEFAULT NULL,
+  `volume` varchar(100) DEFAULT NULL,
+  `tgl_volume` date DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `created_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_sampah`),
+  KEY `id_sungai` (`id_sungai`),
+  CONSTRAINT `tb_sampah_ibfk_1` FOREIGN KEY (`id_sungai`) REFERENCES `tb_sungai` (`id_sungai`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_sampah` */
+
+insert  into `tb_sampah`(`id_sampah`,`id_sungai`,`volume`,`tgl_volume`,`created`,`created_name`) values 
+(1,1,'1263','2020-01-31',NULL,NULL),
+(2,1,'1259','2020-02-28',NULL,NULL),
+(3,1,'1238','2020-03-31',NULL,NULL),
+(4,1,'1212','2020-04-30',NULL,NULL),
+(5,1,'1235','2020-05-31',NULL,NULL),
+(6,1,'1229','2020-06-30',NULL,NULL),
+(7,1,'1195','2020-07-31',NULL,NULL),
+(8,1,'1178','2020-08-30',NULL,NULL),
+(9,1,'1170','2020-09-30',NULL,NULL),
+(10,1,'1167','2020-10-31',NULL,NULL),
+(11,1,'1140','2020-11-30',NULL,NULL),
+(12,1,'1131','2020-12-31',NULL,NULL),
+(13,1,NULL,'2021-01-31',NULL,NULL),
+(14,1,NULL,'2021-02-28',NULL,NULL),
+(15,1,NULL,'2021-03-30',NULL,NULL),
+(16,1,NULL,'2021-04-30',NULL,NULL),
+(17,1,NULL,'2021-05-30',NULL,NULL),
+(18,1,NULL,'2021-06-30',NULL,NULL),
+(19,1,NULL,'2021-07-30',NULL,NULL),
+(20,1,NULL,'2021-08-30',NULL,NULL),
+(21,1,NULL,'2021-09-30',NULL,NULL),
+(22,1,NULL,'2021-10-30',NULL,NULL),
+(23,1,NULL,'2021-11-30',NULL,NULL),
+(24,1,NULL,'2021-12-30',NULL,NULL),
+(25,1,'1311','2022-01-30',NULL,NULL),
+(26,1,'1152','2022-02-28',NULL,NULL),
+(27,1,'1248','2022-03-30',NULL,NULL),
+(28,1,'1206','2022-04-30',NULL,NULL),
+(29,1,'1266','2022-05-30',NULL,NULL),
+(30,1,'1167','2022-06-30',NULL,NULL),
+(31,1,'1242','2022-07-30',NULL,NULL),
+(32,1,'1239','2022-08-30',NULL,NULL),
+(33,1,'1197','2022-09-30',NULL,NULL),
+(34,1,'1275','2022-10-30',NULL,NULL),
+(35,1,'1272','2022-11-30',NULL,NULL),
+(36,1,'1278','2022-12-30',NULL,NULL);
+
+/*Table structure for table `tb_sungai` */
+
+DROP TABLE IF EXISTS `tb_sungai`;
+
+CREATE TABLE `tb_sungai` (
+  `id_sungai` int(11) NOT NULL AUTO_INCREMENT,
+  `id_daerah` int(11) DEFAULT NULL,
+  `nama_sungai` varchar(50) DEFAULT NULL,
+  `letak_sungai` varchar(50) DEFAULT NULL,
+  `status_cctv` enum('aktif','nonaktif') DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `created_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_sungai`),
+  KEY `id_daerah` (`id_daerah`),
+  CONSTRAINT `tb_sungai_ibfk_1` FOREIGN KEY (`id_daerah`) REFERENCES `tb_daerah` (`id_daerah`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_sungai` */
+
+insert  into `tb_sungai`(`id_sungai`,`id_daerah`,`nama_sungai`,`letak_sungai`,`status_cctv`,`created`,`created_name`) values 
+(1,1,'Sg. Hulu Sungai Selatan',' KAB. HULU SUNGAI SELATAN, KALIMANTAN SELATAN','aktif','2023-12-06 16:45:39','adminbase');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
